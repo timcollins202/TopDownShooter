e290f8304e3a54f85a9e6b8370112d7dd120f5f1
Tim <timcollins202@gmail.com>
2025-03-21 14:11:47 -0400

    Tutorial 6

---
+++ TopDownShooter/Source/Gameplay/World/UI.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TopDownShooter.Source.Engine;
+
+namespace TopDownShooter.Source.Gameplay.World
+{
+    public class UI
+    {
+        public SpriteFont font;
+
+        public UI()
+        {
+            font = Globals.content.Load<SpriteFont>("Fonts\\Arial16");
+        }
+
+        public void Update(Source.World world)
+        {
+
+        }
+
+        public void Draw(Source.World world)
+        {
+            string tempStr = "Num Killed = " + world.numKilled;
+            Vector2 strDims = font.MeasureString(tempStr);
+            Globals.spriteBatch.DrawString(
+                font, 
+                tempStr, 
+                new Vector2(Globals.screenWidth / 2 - strDims.X / 2, Globals.screenHeight - 40), 
+                Color.Black);
+        }
+    }
+}
--- TopDownShooter/Source/World.cs
+++ TopDownShooter/Source/World.cs
@@ -9,23 +9,26 @@ namespace TopDownShooter.Source
 {
     public class World
     {
+        public int numKilled;
         public Vector2 offset;
 
         public Hero hero;
         public List<Projectile2d> projectiles;
         public List<Mob> mobs;
         public List<SpawnPoint> spawnPoints;
+        public UI ui;
 
         public World()
         {
             GameGlobals.PassProjectile = AddProjectile;
             GameGlobals.PassMob = AddMob;
 
+            numKilled = 0;
             hero = new Hero("2d\\Hero", new Vector2(300, 300), new Vector2(48, 48));
             projectiles = new List<Projectile2d>();
             mobs = new List<Mob>();
             spawnPoints = new List<SpawnPoint>();
-            offset = new Vector2(0, 0);
+            offset = new Vector2(0, 0);            
 
             //Add spawnPoints, staggering the spawn times by a half second each time
             spawnPoints.Add(new SpawnPoint("2d\\Misc\\circle", new Vector2(50, 50), new Vector2(35, 35)));
@@ -36,18 +39,21 @@ namespace TopDownShooter.Source
             spawnPoints.Add(new SpawnPoint("2d\\Misc\\circle", new Vector2(Globals.screenWidth - 50, 50), new Vector2(35, 35)));
             spawnPoints[spawnPoints.Count - 1].spawnTimer.AddToTimer(1000);
 
+            ui = new UI();
         }
 
         public virtual void Update()
         {
+            //Update Hero
             hero.Update(offset);
 
+            //Update SpawnPoints
             for (int i = 0; i < spawnPoints.Count; i++)
             {
                 spawnPoints[i].Update(offset);
-
             }
 
+            //Update Projectiles
             for (int i=0; i < projectiles.Count; i++)
             {
                 projectiles[i].Update(offset, mobs.ToList<Unit>());
@@ -59,16 +65,20 @@ namespace TopDownShooter.Source
                 }
             }
 
+            //Update Mobs
             for (int i = 0; i < mobs.Count; i++)
             {
                 mobs[i].Update(offset, hero);
 
                 if (mobs[i].dead)
                 {
+                    numKilled++;
                     mobs.RemoveAt(i);
                     i--;
                 }
             }
+
+            ui.Update(this);
         }
 
         public virtual void AddMob(object info)
@@ -99,7 +109,8 @@ namespace TopDownShooter.Source
             {
                 mob.Draw(offset);
             }
-        }
 
+            ui.Draw(this);
+        }
     }
 }
